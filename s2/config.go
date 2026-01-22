4124bfe5f1ad8dfe564fa1ffc207f04a969132b7
Mehul Arora <mehul@s2.dev>
2026-01-22 15:49:20 -0500

    .

--- s2/config.go
+++ s2/config.go
@@ -17,6 +17,7 @@ type Config struct {
 	BasinEndpoint   string // Template (with {basin}) or fixed endpoint, without /v1
 }
 
+// Returns ClientOptions with endpoints from S2_ACCOUNT_ENDPOINT and S2_BASIN_ENDPOINT.
 func LoadConfigFromEnv() *ClientOptions {
 	cfg := loadConfigFromEnv()
 	opts := &ClientOptions{}
